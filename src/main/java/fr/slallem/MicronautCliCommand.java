4b5ad66d840d3473b57774738cb356a5f0b9ec6a
stephane lallemagne <stephane.lallemagne@gmail.com>
2022-04-18 16:28:40 +0200

    Async with non-stopping timeouts

--- src/main/java/fr/slallem/MicronautCliCommand.java
+++ src/main/java/fr/slallem/MicronautCliCommand.java
@@ -8,6 +8,11 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Command(name = "micronaut-cli", description = "...",
         mixinStandardHelpOptions = true)
 public class MicronautCliCommand implements Runnable {
@@ -24,5 +29,112 @@ public class MicronautCliCommand implements Runnable {
         if (verbose) {
             System.out.println("Hi!");
         }
+
+        //-------------------------------------------------------------------------------------
+        // Some simple way to run something asynchronously (no matter when it finishes)
+        //-------------------------------------------------------------------------------------
+
+        System.out.println("---- Demo Part 1 ----");
+        System.out.println("BEFORE ASYNC");
+        new Thread(() -> {
+            doSomethingLong("A", 3);
+        }).start();
+        System.out.println("AFTER ASYNC CALL");
+
+        // You can do other things while Job is still running
+        sleep(5);
+
+        //-------------------------------------------------------------------------------------
+        // Async with CompletableFuture (Java 8+)
+        // You can wait the end of job until some time is out and decide to do other things
+        // if you consider the process too long ; the process still runs in background and
+        // will notify its end with .whenComplete
+        //-------------------------------------------------------------------------------------
+
+        System.out.println("---- Demo Part 2 ----");
+
+        int jobDuration = 9;
+        int waitTime = 3;
+
+        System.out.println("BEFORE ASYNC");
+
+        CompletableFuture<Integer> future = CompletableFuture
+                .supplyAsync(()-> doSomethingLong("B", jobDuration))
+                .whenComplete((res, exc) -> doEndOfJob(res));
+
+        System.out.println("AFTER ASYNC CALL");
+
+        //Wait until async jobs terminates
+        // (or time is out)
+
+        Integer result = -1;
+
+        //Primary wait
+        try {
+            result = future.get(waitTime, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("interrupted exception");
+        } catch (ExecutionException e) {
+            System.out.println("exec exception");
+        } catch (TimeoutException e) {
+            System.out.println("interrupted exception");
+        }
+        System.out.println("At this point result is " + result);
+
+        //Another wait
+        try {
+            result = future.get(waitTime, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("interrupted exception");
+        } catch (ExecutionException e) {
+            System.out.println("exec exception");
+        } catch (TimeoutException e) {
+            System.out.println("interrupted exception");
+        }
+        System.out.println("At this point result is " + result);
+
+
+        System.out.println("Waiting 10 secs before system exit... (let some time to finish running jobs)");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.printf("At the end Result is %s%n", future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("THE REAL END");
+
     }
+
+    public static void sleep(long seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            System.out.println("Sleep has been interrupted!");
+        }
+    }
+
+    public Integer doSomethingLong(String data, int seconds) {
+        Integer i = 0;
+        System.out.printf("Job %s starts%n", data);
+        for (int loop = 0; loop<seconds; loop++) {
+            System.out.printf("Job %s still running...%n", data);
+            sleep(1);
+            i+=10;
+        }
+        System.out.printf("Job %s ends%n", data);
+        return i;
+    }
+
+    public void doEndOfJob(Integer res) {
+        System.out.printf("Job is complete and the result is %s%n", res);
+    }
+
 }
